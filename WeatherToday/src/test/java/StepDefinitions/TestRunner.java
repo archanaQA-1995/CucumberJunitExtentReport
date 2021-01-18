b8d9710adffbfeb06ad65c1c24fbfa643755e593
archanaQA-1995 <77602018+archanaQA-1995@users.noreply.github.com>
2021-01-18 12:48:09 +0800

    Add files via upload

---
+++ WeatherToday/src/test/java/StepDefinitions/TestRunner.java
@@ -0,0 +1,17 @@
+package StepDefinitions;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src\\test\\resources\\Features",glue={"StepDefinitions"},
+       monochrome = true,
+        dryRun = false,
+        plugin = { "pretty", "html:target/cucumber-reports/report.html"})
+public class TestRunner {
+// To generate the HTML pretty report and this is the runner class
+}
